b1a0c04312a650aa563c545249575f294d0818dd
mohsen <hasanimohammadhosein@gmail.com>
2021-12-09 23:42:11 +0330

    create Group and Participant models

--- Group/models.py
+++ Group/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from AuthApp.models import UserProfile
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Participant(models.Model):
+    role = (
+        ("O", "Owner"),
+        ("A", "Admin"),
+        ("M", "Member"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
---
+++ Group/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    # path('', Home.as_view(), name='home'),
+    # path('signup/', SignUpView.as_view(), name='signup'),
+    # url('profile/', EditUserProfileView.as_view(), name="edit-user-profile"),
+]
